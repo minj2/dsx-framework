e828a02d448ef8872f7ca95979c4fec57fdb0d5a
minj2 <minj2@hotmail.com>
2025-02-22 00:08:23 -0800

    Create theme.js

---
+++ storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "DSX Framework",
+  brandUrl: "https://github.com/minj2/dsx-framework",
+  brandImage: "https://your-logo-url.com/logo.png", // 로고 이미지 추가 가능!
+});
